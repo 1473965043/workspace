da1c9cda89ff734f8550cb189993ff0996807c1a
guodong <1473965043@qq.com>
2017-03-22 10:30:59 +0800

    测试

---
+++ app/src/main/java/com/hq/fiveonejrq/jrq/homepage/A.java
@@ -0,0 +1,17 @@
+package com.hq.fiveonejrq.jrq.homepage;
+
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+
+public class A {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- app/src/main/java/com/hq/fiveonejrq/jrq/homepage/SearchActivity.java
+++ app/src/main/java/com/hq/fiveonejrq/jrq/homepage/SearchActivity.java
@@ -10,6 +10,7 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        A a = new A();
         getSupportActionBar().hide();
         initView();
         initEvents();
